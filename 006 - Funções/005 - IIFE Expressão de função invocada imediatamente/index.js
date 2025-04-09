9e0cac73ad2a44e12eb1c46038aa0d0f28a8e012
unknown <renanalencar02@gmail.com>
2025-04-09 16:40:22 -0300

    feat: modulo de funcoes incompleto

---
+++ 006 - Funções/005 - IIFE Expressão de função invocada imediatamente/index.js
@@ -0,0 +1,26 @@
+/*
+    IIFE - Immediately Invoked Function Expression
+    Uma função que é invocada imediatamente após ser definida.
+
+*/
+
+// IIFE com parâmetros
+(function(num1, num2) {
+    const calc = num1 + num2;
+    console.log("IIFE com parâmetros")
+    console.log(calc) // 3
+}(1,2));
+
+// IIFE com arrow function
+(() => {
+    var teste = "IIFE com arrow function"
+    console.log(teste) // IIFE com arrow function
+})();
+
+// console.log(teste) // ReferenceError: teste is not defined
+
+// IIFE com parâmetros e arrow function
+((nome) => {
+    console.log("IIFE com parâmetros e arrow function")
+    console.log(nome)
+})("Renan Alencar") // Renan Alencar;
